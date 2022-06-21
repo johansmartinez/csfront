a6f2d2c76a96de4f00f09781a39e7aee8e0f5e86
johsnsmartinez <johan.martinez.dev@gmail.com>
2022-06-21 17:55:16 -0500

    panel de asistencia

--- src/portal/attendance/Attendance.js
+++ src/portal/attendance/Attendance.js
@@ -1,11 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect,useContext } from 'react';
 import Calendar from 'rc-year-calendar';
+import { UserContext } from "../../context/UserProvider";
+import { SERVER } from '../../context/Backend';
 import 'rc-year-calendar/locales/rc-year-calendar.es';
+import axios from 'axios';
 
 function Attendance() {
-    const [days, setDays] = useState(['2022-06-04', new Date('2022-06-05'),new Date('2022-06-07')])
+    const { user,getToken }= useContext(UserContext);
+    const [days, setDays] = useState([])
 
     const data=days.map(d=> {return {startDate:new Date(d), endDate:new Date(d), color:'#fe6601'}})
+    useEffect(() => {
+        const config={
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+                'token-clubsue':getToken()
+            }
+        }
+        axios.get(`${SERVER}/attendance/${user.documento}`, config)
+        .then(({data})=>{
+            alert(JSON.stringify(data))
+            let a=data.map(e=> e.fecha_asistencia)
+            setDays(a)
+        }).catch(()=>{})
+    }, [])
+    
     return (
         <>
             <Calendar
--- src/portal/attendance/RegisterAttendance.js
+++ src/portal/attendance/RegisterAttendance.js
@@ -1,30 +1,74 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../context/UserProvider";
+import { SERVER } from '../../context/Backend';
+import Swal from "sweetalert2";
 
 function RegisterAttendance() {
 
-    const [student, setStudent] = useState('');
-    const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
+    const { user,getToken }= useContext(UserContext);
+    const [search, setSearch] = useState('');
+    const [students, setStudents] = useState([]);
+    let filter=students.filter(e=>{
+        const nc=e.nombres+' '+e.apellidos;
+        return String(nc).toUpperCase().includes(search.toUpperCase())
+    })
+    let array=(search==='')?students:filter;
+    
 
-    const handleSend=()=>{
-        
+    useEffect(() => {
+        const config={
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+                'token-clubsue':getToken()
+            }
+        }
+        axios.get(`${SERVER}/attendance/`, config)
+        .then(({data})=>{
+            setStudents(data)
+        }).catch(()=>{})
+    }, [])
+    
+    const registerAttendance=(id)=>{
+        const config={
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+                'token-clubsue':getToken()
+            }
+        }
+        axios.post(`${SERVER}/attendance/`, {instructor: user.documento, alumno:id},config)
+        .then(({data})=>{
+            setStudents(data)
+            Swal.fire({
+                icon: 'success',
+                text: `Asistencia registrada`,
+                confirmButtonColor:'#fe6601'
+            });
+        }).catch(()=>{
+            Swal.fire({
+                icon: 'error',
+                text: `No se ha podido registrar la asistencia`,
+                confirmButtonColor:'#fe6601'
+            });
+        })
     }
 
     return (
         <>
             <form className="form-normal">
                 <h2 className="title">Registrar asistencia</h2> 
-                <p className="text-normal">Por favor rellene los campos obligatorios (<span className="red">*</span>). </p>
+                <p className="text-normal">A continuación se listan los estudiantes para registrar su asistencia</p>
                 <label className="field-form">
-                    <p>Fecha de asistencia:<span className="red">*</span></p>
-                    <input type='date' placeholder="Ingrese la fecha" value={date} onChange={e=>{setDate(e.target.value)}}/>
+                    <input type='text' placeholder="Ingrese nombre de estudiante para buscar" value={search} onChange={e=>{setSearch(e.target.value)}}/>
                 </label>
-                <label className="field-form">
-                    <p>Documento del estudiante:<span className="red">*</span></p>
-                    <input type='text' placeholder="Ingrese el número de documento del estudiante" value={student} onChange={e=>{setStudent(e.target.value)}}/>
-                </label>
-                <button type="button" className="form-orange-button" onClick={handleSend}>
-                    REGISTRAR
-                </button>
+                {
+                    array.map(e=>(
+                        <div className="attendance-div">
+                            <span>{e.documento} - {e.nombres} {e.apellidos}</span>
+                            <button className="orange-button" onClick={()=>registerAttendance(e.documento)}>REGISTRAR</button>
+                        </div>
+                    ))
+                }
             </form>
         </>
     );
